587d7424b3809ce4085b4dd7007790712ff1f8e4
Tomonori Ueno <t.uepon@gmail.com>
2018-08-29 23:37:02 +0900

    32: "Photo uploads" is OK

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Album;
 
 
 class AlbumsController extends Controller
 {
     public function index(){
-        return view('albums.index');
+        $albums = Album::with('Photos')->get();
+        return view('albums.index')->with('albums', $albums);
     }
 
     public function create(){
@@ -36,6 +38,14 @@ class AlbumsController extends Controller
         // Upload image
         $path = $request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
 
-        return $path;
+        // Create album
+        $album = new Album;
+        $album->name = $request->input('name');
+        $album->description = $request->input('description');    
+        $album->cover_image = $filenameToStore;
+        
+        $album->save();
+
+        return redirect('/albums')->with('success', 'Album Created');
     }
 }
