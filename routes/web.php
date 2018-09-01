580488b64228ee73aa00ad6a3639ae812ad2d7a3
Tomonori Ueno <t.uepon@gmail.com>
2018-09-01 14:57:44 +0900

    DELETE does not work

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', 'AlbumsController@index');
 Route::get('/albums', 'AlbumsController@index');
 Route::get('/albums/create', 'AlbumsController@create');
 Route::get('/albums/{id}', 'AlbumsController@show');
+/* 19: Add 'get' to fix 'post' does not work */ 
 Route::get('/albums/store', 'AlbumsController@store');
 Route::post('/albums/store', 'AlbumsController@store');
 
 Route::get('/photos/create/{id}', 'PhotosController@create');
 Route::post('/photos/store', 'PhotosController@store');
+Route::get('/photos/{id}', 'PhotosController@show');
+Route::delete('/photos/{id}', 'PhotosController@destroy');
